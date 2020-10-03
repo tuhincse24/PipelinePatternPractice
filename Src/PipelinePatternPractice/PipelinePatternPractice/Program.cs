0ef417dc0b13f71a2cd3c5583086390dda65c5bc
Tuhin <towhid.tuhin@selise.ch>
2020-10-03 22:24:41 +0600

    PipeLine test for large list.

--- Src/PipelinePatternPractice/PipelinePatternPractice/Program.cs
+++ Src/PipelinePatternPractice/PipelinePatternPractice/Program.cs
@@ -1,5 +1,6 @@
 ﻿using PipelinePatternPractice.Infrastructure;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,18 +13,34 @@ namespace PipelinePatternPractice
             //GenericPipeline();
             var pipeline = CreateGenericPipeline();
 
-            var tsk = Task.Run(async () =>
-            {
-                Console.WriteLine(await pipeline.Execute("The pipeline pattern1 is the best pattern1"));
-                Console.WriteLine(await pipeline.Execute("The pipeline pattern2 is the best pattern2"));
-                Console.WriteLine(await pipeline.Execute("The pipeline pattern3 is the best pattern3"));
-                Console.WriteLine(await pipeline.Execute("The pipeline patter4 is the best patter4"));
-                Console.WriteLine(await pipeline.Execute("The pipeline pattern5 is The best pattern5 pattern5 The"));
-            });
-            tsk.Wait();
+            //var tsk = Task.Run(async () =>
+            //{
+            //    Console.WriteLine(await pipeline.Execute("The pipeline pattern1 is the best pattern1"));
+            //    Console.WriteLine(await pipeline.Execute("The pipeline pattern2 is the best pattern2"));
+            //    Console.WriteLine(await pipeline.Execute("The pipeline pattern3 is the best pattern3"));
+            //    Console.WriteLine(await pipeline.Execute("The pipeline patter4 is the best patter4"));
+            //    Console.WriteLine(await pipeline.Execute("The pipeline pattern5 is The best pattern5 pattern5 The"));
+            //});
 
+            //tsk.Wait();
+
+            var taskList = new List<Task>();
+            for(int i = 0; i < 10000; i++)
+            {
+                var task = pipeline.Execute($"The pipeline pattern{i} is the best patter{i}");
+                taskList.Add(task);
+            }
+            Task.Run(async () =>
+            {
+                await Task.WhenAll(taskList);
+            }).Wait();
 
             Console.Read();
+        }
+
+        private static object List<T>()
+        {
+            throw new NotImplementedException();
         }
 
         private static GenericPipeline<string, bool> CreateGenericPipeline()
@@ -37,11 +54,6 @@ namespace PipelinePatternPractice
 
         private static string FindMostCommon(string input)
         {
-
-            var mostFrequentWord1 = input.Split(' ')
-                .GroupBy(word => word)
-                .OrderBy(group => group.Count()).ToList();
-
             var mostFrequentWord = input.Split(' ')
                 .GroupBy(word => word)
                 .OrderBy(group => group.Count())
@@ -53,11 +65,13 @@ namespace PipelinePatternPractice
 
         private static int CountChars(string mostCommon)
         {
+            Console.WriteLine(mostCommon);
             return mostCommon.Length;
         }
 
         private static bool IsOdd(int number)
         {
+            Console.WriteLine(number.ToString());
             var res = number % 2 == 1;
             Console.WriteLine(res.ToString());
             return res;
